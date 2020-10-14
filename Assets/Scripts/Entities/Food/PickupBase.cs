4fe9bc69fefcc2121e5096c030bbc4e381e620f2
Bazztoner <bazztoner.phoenix@gmail.com>
2020-10-14 00:35:06 -0300

    Initial Commit
    3C casi completo

---
+++ Assets/Scripts/Entities/Food/PickupBase.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PickupBase : MonoBehaviour, IInteractuable
+{
+    public Transform playerHand;
+    PlayerController _player;
+    bool _isPickup;
+
+    Rigidbody _rb;
+    Collider _coll;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _coll = GetComponent<Collider>();
+
+    }
+
+    void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+        playerHand = _player.hand.transform;
+
+        Physics.IgnoreCollision(_player.GetComponent<Collider>(), _coll);
+    }
+
+    void Update()
+    {
+        if (_isPickup)
+        {
+            transform.position = playerHand.position;
+        }
+    }
+
+    public void Interact()
+    {
+        if (!_isPickup)
+        {
+            _isPickup = true;
+            ChangePhysicsState(false);
+            transform.parent = playerHand;
+        }
+    }
+
+    public void Throw(float force)
+    {
+        transform.parent = null;
+        _isPickup = false;
+        ChangePhysicsState(true);
+        _rb.AddForce(_player.cam.transform.forward * force, ForceMode.VelocityChange);
+    }
+
+    public void Drop()
+    {
+        transform.parent = null;
+        _isPickup = false;
+        ChangePhysicsState(true);
+    }
+
+    void ChangePhysicsState(bool physicsOn)
+    {
+        if (physicsOn)
+        {
+            _rb.useGravity = true;
+            _rb.isKinematic = false;
+            _coll.isTrigger = false;
+        }
+        else
+        {
+            _rb.useGravity = false;
+            _rb.isKinematic = true;
+            _coll.isTrigger = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    public float movementSpeed;
+    public float runMultiplier;
+
+    public float cameraRotationClamp = 30f;
+    public float headSensitivity;
+    public float headRotationSpeed;
+
+    bool _lockedByGame;
+    bool _hasItem;
+
+    PickupBase _itemPickup;
+    public PlayerHand hand;
+
+    Keyboard _keyboard;
+    Gamepad _gamepad;
+    Mouse _mouse;
+    public Camera cam;
+
+    Tuple<float, float> _directionVectors = new Tuple<float, float>(0, 0);
+
+    Rigidbody _rb;
+    #endregion
+
+    void Awake()
+    {
+        _keyboard = Keyboard.current;
+        _gamepad = Gamepad.current;
+        _mouse = Mouse.current;
+        cam = GetComponentInChildren<Camera>();
+        _rb = GetComponent<Rigidbody>();
+        hand = GetComponentInChildren<PlayerHand>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (_lockedByGame) return;
+
+        MovementInput();
+        MouseLook();
+        CheckInteract();
+        CheckMouseInput();
+    }
+
+    void FixedUpdate()
+    {
+        if (_lockedByGame) return;
+
+        CheckMovement();
+    }
+
+
+    void MovementInput()
+    {
+        var xVkt = _keyboard.dKey.isPressed ? 1 : _keyboard.aKey.isPressed ? -1 : 0;
+        var zVkt = _keyboard.wKey.isPressed ? 1 : _keyboard.sKey.isPressed ? -1 : 0;
+
+        _directionVectors = new Tuple<float, float>(xVkt, zVkt);
+    }
+
+    void CheckMovement()
+    {
+        var dir = transform.forward * _directionVectors.Item2 + transform.right * _directionVectors.Item1;
+        var multiplier = _keyboard.leftAltKey.isPressed || _keyboard.leftShiftKey.isPressed ? runMultiplier : 1;
+        var movVector = _rb.position + dir.normalized * Time.fixedDeltaTime * (movementSpeed * multiplier);
+        _rb.MovePosition(movVector);
+    }
+
+    void MouseLook()
+    {
+        var mouseDelta = _mouse.delta.ReadValue() * Time.deltaTime * headSensitivity;
+
+        Vector3 headRot = cam.transform.localEulerAngles + new Vector3(-mouseDelta.y, 0, 0f);
+        headRot.x = ClampAngle(headRot.x, -cameraRotationClamp, cameraRotationClamp);
+        cam.transform.localRotation = Quaternion.Euler(headRot);
+
+        Vector3 playerRot = transform.eulerAngles + new Vector3(0, mouseDelta.x, 0f);
+        transform.rotation = Quaternion.Euler(playerRot);
+    }
+
+    float ClampAngle(float angle, float from, float to)
+    {
+        // accepts e.g. -80, 80
+        if (angle < 0f) angle = 360 + angle;
+        if (angle > 180f) return Mathf.Max(angle, 360 + from);
+        return Mathf.Min(angle, to);
+    }
+
+    void CheckMouseInput()
+    {
+        if (_hasItem)
+        {
+            if (_mouse.leftButton.wasPressedThisFrame)
+            {
+                _itemPickup.Throw(8f);
+                _hasItem = false;
+                _itemPickup = null;
+            }
+            else if(_mouse.rightButton.wasPressedThisFrame)
+            {
+                _itemPickup.Drop();
+                _hasItem = false;
+                _itemPickup = null;
+            }
+        }
+    }
+
+    void CheckInteract()
+    {
+        if (_keyboard.fKey.wasPressedThisFrame)
+        {
+            if (_hasItem)
+            {
+                _itemPickup.Drop();
+                _hasItem = false;
+                _itemPickup = null;
+            }
+            else
+            {
+                RaycastHit rch;
+                var mask = LayerMask.GetMask("Interactuable");
+                var hits = Physics.Raycast(cam.transform.position, cam.transform.forward, out rch, 4, mask);
+                if (hits)
+                {
+                    print(rch.transform.name);
+
+                    if (rch.collider.GetComponent(typeof(IInteractuable)) is IInteractuable interact) interact.Interact();
+
+                    var pickup = rch.collider.GetComponent<PickupBase>();
+                    if (pickup)
+                    {
+                        _hasItem = true;
+                        _itemPickup = pickup;
+                    }
+                }
+            }
+        }
+    }
+}
