24f50c931dbcb5580e43f37fb03f58d1ace02ff1
Bazztoner <bazztoner.phoenix@gmail.com>
2021-01-12 01:36:38 -0300

    PANCITO UPDATE
    El tupper ahora es un pancito con animación

---
+++ Assets/Scripts/Entities/Customer/BreadDeliveryObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BreadDeliveryObject : OrderDelivery
+{
+    Animator _an;
+    Renderer[] _rends;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _an = GetComponent<Animator>();
+        _rends = GetComponentsInChildren<Renderer>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        _an.SetBool("hasFood", foodToDeliver != null);
+    }
+
+    public override void ActivateHighlight(bool state)
+    {
+        foreach (var renderer in _rends)
+        {
+            renderer.material.SetFloat("_Highlighted", state ? 1f : 0f);
+        }
+    }
+}
